adc5278a6a10ff6a72743dc62594ce10d4f2819b
Fsoky <cyberuest0x12@gmail.com>
2024-01-06 16:04:19 +0600

    Annotations +new buttons

--- vktools/keyboard.py
+++ vktools/keyboard.py
@@ -1,15 +1,15 @@
 import json
 from enum import Enum
-from typing import List, Dict, Any, Union, Optional
+from typing import Any
 
 
 class ButtonColor(Enum):
-    NEGATIVE: str = "negative"
-    POSITIVE: str = "positive"
-    PRIMARY: str = "primary"
-    SECONDARY: str = "secondary"
+    NEGATIVE = "negative"
+    POSITIVE = "positive"
+    PRIMARY = "primary"
+    SECONDARY = "secondary"
 
-    def default(self):
+    def __str__(self) -> str:
         return self.value
 
 
@@ -18,16 +18,35 @@ class Text:
     def __new__(
         cls,
         label: str,
-        color: ButtonColor=ButtonColor.SECONDARY,
-        payload: Optional[str] | None=None
-    ) -> Optional[Dict[str, Union[str, Dict[str, str], None]]]:
+        color: str | ButtonColor=ButtonColor.SECONDARY,
+        payload: str | None=None
+    ) -> dict[str, Any] | None:
+        """
+        Text button
+
+        :param label: Button Text. Will be sent by the user to the community dialog
+        or conversation when clicked. Maximum length is 40 characters.
+        :param color: Button color. The parameter is used only for text and callback buttons.
+        Possible values are given below, in the Key Colors section.
+        :param payload: Additional information (JSON). In buttons that do not send text,
+        it is passed for compatibility with older clients. The maximum is 255 characters.
+
+        - Usage:
+
+        .. code-block:: python
+            Text(
+                label="My Button",
+                color=ButtonColor.PRIMARY, # or "primary"
+            )
+        """
+
         return {
             "action": {
                 "type": "text",
                 "label": label,
                 "payload": payload
             },
-            "color": color.value
+            "color": color.value if isinstance(color, ButtonColor) else color
         }
 
 
@@ -35,10 +54,27 @@ class OpenLink:
 
     def __new__(
         cls,
-        label: Optional[str],
-        link: Optional[str],
-        payload: Optional[str] | None=None
-    ) -> Optional[Dict[str, Union[str, Dict[str, str], None]]]:
+        label: str | None,
+        link: str | None,
+        payload: str | None=None
+    ) -> dict[str, Any] | None:
+        """
+        Open link button
+
+        :param label: Button Text.
+        :param link: The link to be opened by clicking on the button.
+        :param payload: Additional information (JSON). In buttons that do not send text,
+        it is passed for compatibility with older clients. The maximum is 255 characters.
+
+        - Usage:
+
+        .. code-block:: python
+            OpenLink(
+                label="My Button",
+                link="https://t.me/fsoky_community"
+            )
+        """
+
         return {
             "action": {
                 "type": "open_link",
@@ -53,8 +89,15 @@ class Location:
 
     def __new__(
         cls,
-        payload: Optional[str] | None=None
-    ) -> Optional[Dict[str, Union[str, Dict[str, str], None]]]:
+        payload: str | None=None
+    ) -> dict[str, Any] | None:
+        """
+        Location button
+
+        :param payload: Additional information (JSON). It will be returned in the message_new event
+        in the field of the same name. Maximum - 255 characters.
+        """
+
         return {
             "action": {
                 "type": "location",
@@ -68,8 +111,18 @@ class VkPay:
     def __new__(
         cls,
         pay_hash: str,
-        payload: Optional[str] | None=None
-    ) -> Optional[Dict[str, Union[str, Dict[str, str], None]]]:
+        payload: str | None=None
+    ) -> dict[str, Any] | None:
+        """
+        Vk Pay button
+
+        :param pay_hash: A string containing VK Pay payment parameters and application ID in aid parameter ,
+        separated by &. Example: `action=transfer-to-group&group_id=1&aid=10`.
+        The string must not contain the # symbol itself.
+        :param payload: Additional information (JSON). It will be returned in the message_new event
+        in the field of the same name. Maximum - 255 characters.
+        """
+
         return {
             "action": {
                 "type": "vkpay",
@@ -87,8 +140,22 @@ class VkApps:
         owner_id: int,
         label: str,
         app_hash: str,
-        payload: Optional[str] | None=None
-    ) -> Optional[Dict[str, Union[str, Dict[str, Any], None]]]:
+        payload: str | None=None
+    ) -> dict[str, Any] | None:
+        """
+        Vk Apps button
+
+        :param app_id: The identifier of the called application with the type VK Mini Apps.
+        :param owner_id: Identifier of the community in which the application is installed,
+        if required to open in a community context.
+        :param label: The name of the application indicated on the button.
+        :param app_hash: The hash for navigation in the application,
+        will be passed in the startup parameter string after the # character.
+        Do not add the # character itself to the hash.
+        :param payload: Additional information (JSON). It will be returned in the message_new event
+        in the field of the same name. Maximum - 255 characters.
+        """
+
         return {
             "action": {
                 "type": "vkapps",
@@ -99,32 +166,96 @@ class VkApps:
                 "payload": payload
             }
         }
-    
+
+
+class OpenApp:
+
+    def __new__(
+        cls,
+        app_id: int,
+        owner_id: int,
+        label: str,
+        app_hash: str,
+        payload: str | None=None,
+    ) -> dict[str, Any] | None:
+        """
+        Open app button
+
+        :param app_id: The identifier of the called application with the type VK Mini Apps.
+        :param owner_id: Identifier of the community in which the application is installed,
+        if required to open in a community context.
+        :param label: The name of the application indicated on the button.
+        :param app_hash: The hash for navigation in the application,
+        will be passed in the startup parameter string after the # character.
+        Do not add the # character itself to the hash.
+        :param payload: Additional information (JSON). In buttons that do not send text,
+        it is passed for compatibility with older clients. The maximum is 255 characters.
+        """
+
+        return {
+            "action": {
+                "type": "open_app",
+                "app_id": app_id,
+                "owner_id": owner_id,
+                "label": label,
+                "hash": app_hash,
+                "payload": payload
+            }
+        }
+
+
+class Callback:
+
+    def __new__(
+        cls,
+        label: str,
+        payload: str | None=None
+    ) -> dict[str, Any] | None:
+        """
+        Callback button
+
+        :param label: Button text. The maximum length is 40 characters.
+        :param payload: Additional information (JSON) returned in the `message_event` event
+        in the field of the same name. Maximum - 255 characters.
+        """
+
+        return {
+            "action": {
+                "type": "callback",
+                "label": label,
+                "payload": payload
+            }
+        }
+
 
 class Keyboard:
 
     def __init__(
         self,
-        button: List[
-            Union[
-                Text,
-                OpenLink,
-                Location,
-                VkPay,
-                VkApps
-            ]
+        buttons: list[
+            Text | OpenLink | Location | VkPay | VkApps | OpenApp | Callback
         ],
         *,
         one_time: bool=False,
         inline: bool=False
     ) -> None:
+        """
+        Keyboard
+
+        :param buttons: List of buttons
+        :param one_time: Whether to hide the keyboard after pressing a key that sends a message.
+        For example: `text` or `location`. Only works for `"inline": false`.
+        :param inline: `true` - the keyboard is displayed inside the message. 
+        `false` - displays the keyboard in the dialog box, under the message input field.
+        """
+
         self.one_time = one_time
         self.inline = inline
 
-        self.keyboard: Dict[str, Union[bool, List[Union[Text, OpenLink, Location, VkPay, VkApps]]]] = {
+        self.keyboard = {
             "one_time": self.one_time,
             "inline": self.inline,
-            "buttons": button
+            "buttons": buttons
         }
 
     def add_keyboard(self) -> str:
